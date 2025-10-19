253dde9bb19ef5a234f551573577b87862a3503e
kellen xavier <kellenxaviier@gmail.com>
2025-10-19 02:08:52 -0300

    fix: gerar apk android - configurar comandos

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'HortiAssistente',
-  webDir: 'dist'
+  appId: 'com.example.app',
+  appName: 'hortiassistente',
+  webDir: 'build'
 };
 
 export default config;
